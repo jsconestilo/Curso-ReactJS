7f9929374806023b53b2ab765f1572c4955b09ce
Alejandro González Reyes <jsconestilo@gmail.com>
2019-05-05 18:16:23 -0500

    Explicacion JSX vs React.createElement

--- src/index.js
+++ src/index.js
@@ -15,15 +15,67 @@ serviceWorker.unregister();*/
 import React from "react";
 import ReactDom from "react-dom";
 
+// const element = <h1>Hola Mundo desde ReactJS</h1>;
+
 /**
- * Esto es JSX, es un lenguaje para representar HTML directamente desde Javascript
- * Es poderoso ya que podemos interpolar contenido de variables y sentencias
- * Es una nueva caracteristica de JS, por tanto se requiere de Babel (incluido).
- * Para ello se emplea el módulo "react" (Es un anallogo a createElement de JS nativo)
+ * EXPLICACION 1:
+ * JSX es una extensión de JavaScript creada por Facebook para el uso con la biblioteca React. Sirve de preprocesador (como Sass o Stylus a CSS) y transforma el código generado con React a JavaScript. (jsx solo es azucar sintetico)
+ * JSX tiene su alternativa que es React.createElement pero es preferible JSX porque es mucho más legible y expresivo. Ambos tienen el mismo poder y la misma capacidad.
  *
- * Siempre que escribas JSX es requisito importar React.
+ * (__elemento__, __atributos__, __hijo1__, [__hijo2__, __hijoN__])
  */
-const element = <h1>Hola Mundo desde ReactJS</h1>;
+
+//const element = React.createElement("h1", {}, "Hola, soy los children");
+
+/*
+const element = React.createElement(
+  "a",
+  { href: "http://platzi.com" },
+  "Ir a Platzi"
+);
+*/
+
+const nombre = "Alejandro González";
+const suma = function() {
+  return 3 + 3;
+};
+
+/**
+ * EXPLICACION 2:
+ * React.createElement() y JSX pueden mostrar información de variables e instrucciiones de Javascript
+ * con createElement es necesario literal template ${variable}
+ * con JSX solo es necesario declarar { }
+ * Las expresiones pueden ser llamadas a otras funciones, cálculos matemáticos, etc. Si las expresiones son false, 0, null, undefined, entre otros, no se verán.
+ */
+
+//const element = React.createElement("h1", {}, `Hola, soy ${nombre}`);
+
+/*const element = (
+  <h1>
+    Hola soy {nombre} y tengo {2019 - 1985} años y la función tiene {suma()}.
+    variables con contenido null, undefined, false, no se mostrarán {null}
+  </h1>
+);*/
+
+/**
+ * EXPLICACION 3:
+ * El siguiente ejemplo demuestra porque se prefiere emplear JSX en lugar de React.createElement
+ * JSX es un lenguaje más expresivo y simple de utilizar
+ */
+
+const jsx = (
+  <div>
+    <h1>Hola soy {nombre}</h1>
+    <p>Soy ingeniero en FrontEnd</p>
+  </div>
+);
+
+const element = React.createElement(
+  "div",
+  {},
+  React.createElement("h1", {}, `Hola, soy ${nombre}`),
+  React.createElement("p", {}, "Soy licenciado en informática")
+);
 
 // En este punto localizo el contenedor padre donde montaré mi App de Reactjs
 const container = document.getElementById("root");

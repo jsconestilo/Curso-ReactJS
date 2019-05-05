49b0e43b972efaaa3a4b1f9e94e485f0f6cd56c9
Alejandro González Reyes <jsconestilo@gmail.com>
2019-05-05 17:48:37 -0500

    Explicacion de ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -9,4 +9,28 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister();*/
+
+// Librerías necesarias para aplicaciones React
+import React from "react";
+import ReactDom from "react-dom";
+
+/**
+ * Esto es JSX, es un lenguaje para representar HTML directamente desde Javascript
+ * Es poderoso ya que podemos interpolar contenido de variables y sentencias
+ * Es una nueva caracteristica de JS, por tanto se requiere de Babel (incluido).
+ * Para ello se emplea el módulo "react" (Es un anallogo a createElement de JS nativo)
+ *
+ * Siempre que escribas JSX es requisito importar React.
+ */
+const element = <h1>Hola Mundo desde ReactJS</h1>;
+
+// En este punto localizo el contenedor padre donde montaré mi App de Reactjs
+const container = document.getElementById("root");
+
+/**
+ * Para mostrar el elemento en pantalla, es necesario renderizarlo.
+ * Es lo que hace react-dom (es un analogo a appendChild() nativo de JS)
+ * sus parametros son: (__queElemento__, __donde__)
+ */
+ReactDom.render(element, container);
